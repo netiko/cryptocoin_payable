dc018de005e6eecad085a9499c26ef1c22bdf14e
netiko <contact@netiko.fr>
2019-10-07 04:39:14 +0200

    fix ltc explorer

--- lib/cryptocoin_payable/adapters/litecoin.rb
+++ lib/cryptocoin_payable/adapters/litecoin.rb
@@ -5,9 +5,14 @@ module CryptocoinPayable
         'LTC'
       end
 
-      def fetch_transactions(address)
+      def fetch_transactions_chain_so(address)
 
         url = "https://chain.so/api/v2/address/LTC#{prefix}/#{address}"
+        parse_block_explorer_transactions_chain_so(get_request(url).body, address)
+      end
+
+      def fetch_transactions(address)
+        url = "https://api.blockcypher.com/v1/ltc/main/addrs/#{address}/full"
         parse_block_explorer_transactions(get_request(url).body, address)
       end
 
@@ -25,15 +30,15 @@ module CryptocoinPayable
         output_transactions ? (output_transactions['value'].to_f * self.class.subunit_in_main).to_i : 0
       end
 
-      def parse_block_explorer_transactions(response, address)
+      def parse_block_explorer_transactions_chain_so(response, address)
         json = JSON.parse(response)
         raise ApiError, json['message'] if json['code'] == 404
-        json['data']['txs'].reject { |tx| tx['incoming'].nil? }.map { |tx| convert_block_explorer_transactions(tx, address) }
+        json['data']['txs'].reject { |tx| tx['incoming'].nil? }.map { |tx| convert_block_explorer_transactions_chain_so(tx, address) }
       rescue JSON::ParserError
         raise ApiError, response
       end
 
-      def convert_block_explorer_transactions(transaction, address)
+      def convert_block_explorer_transactions_chain_so(transaction, address)
         {
           transaction_hash: transaction['txid'],
           block_hash: nil, # Not supported
